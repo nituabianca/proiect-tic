ba1356aba6c7c0b137cd7f1eac2ac3365df2ed35
Bibboo <73643206+nituabianca@users.noreply.github.com>
2025-06-17 15:14:44 +0300

    Update index.js for the new library route

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,7 @@ const ratingRoutes = require("./ratings");
 const orderRoutes = require("./orders");
 const recommendationRoutes = require("./recommendations");
 const statisticsRoutes = require("./statistics");
+const libraryRoutes = require("./library")
 
 const router = express.Router();
 
@@ -17,5 +18,6 @@ router.use("/ratings", ratingRoutes);
 router.use("/orders", orderRoutes);
 router.use("/recommendations", recommendationRoutes);
 router.use("/statistics", statisticsRoutes);
+router.use("/library", libraryRoutes);
 
 module.exports = router;
